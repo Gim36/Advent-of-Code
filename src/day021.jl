80b0fed25581a9cb6f59fc57254c260b535a31d0
Ryoschin <nikolagagic5127@gmail.com>
2024-12-04 10:11:47 +0100

    0.0.3

--- src/day021.jl
+++ src/day021.jl
@@ -1,21 +1,35 @@
 module julia_2024
 
+function is_safe(input::String)
+    parts = split(input)
+    last = parse(Int, parts[2])
+    difference = last - parse(Int, parts[1])
+    if (difference > 0 || difference < 0)
+        for i in 3:lastindex(parts)
+            part = parse(Int, parts[i])
+            difference = abs(part - last)
+            if (difference < 1 || difference > 3)
+                return false
+            end
+            last = parts[i]
+        end
+    else
+        return false
+    end
+
+    return true
+end
+
 function day02()
+    counter = 0
     while true
         input = readline()
         if input == ""
-            break  # End of input
+            return counter
         end
 
-        parts = split(input)
-        last = parse(Int, parts[2])
-        difference = last - parse(Int, parts[1])
-        if (difference > 0)
-            for i in 3:lastindex(parts)
-                part = parse(Int, parts[i])
-                difference = part - last
-                last = parts[i]
-            end
+        if is_safe(input)
+            counter += 1
         end
     end
 end
